9ecda635be36fadc5b50490f96e30c40093ae0aa
Ludovic <ludovictichit3@gmail.com>
2016-10-30 11:14:42 +0100

    [LTI]Adding retrieving and updating methods on user (including an new id field)

--- routes/account.js
+++ routes/account.js
@@ -6,12 +6,22 @@ var _ = require('lodash');
 
 var users = require ("../config").users;
 
+router.get('/account/:id', function(req, res) {
+	users.get(req.params.id, function(user, err) {
+		if(err) {
+			console.log(err.cause);
+			res.status(err.status).send(err.cause);
+		} else {
+			res.status(200).json(user);
+		}
+	});
+});
+
 router.post('/account/signup', function(req, res) {
 	if(!req.body.email || !req.body.firstname || !req.body.surname) {
 		res.status(401).send("Required email/firstname/surname");
 	} else {
-        var bio = _.isUndefined(req.body.biography) ? '' : req.body.biography;
-		users.signup(req.body.email, req.body.firstname, req.body.surname, bio, function(user, err){
+		users.signup(req.body.email, req.body.firstname, req.body.surname, req.body.biography, function(user, err){
 			if(err) {
 				console.log(err.cause);
 				res.status(err.status).send(err.cause);
@@ -20,6 +30,17 @@ router.post('/account/signup', function(req, res) {
 			}
 		});
 	}
+});
+
+router.put('/account/:id', function(req, res) {
+	users.update(req.params.id, req.body.email, req.body.firstname, req.body.surname, req.body.biography, function(user, err) {
+		if(err) {
+			console.log(err.cause);
+			res.status(err.status).send(err.cause);
+		} else {
+			res.status(200).send('User updated');
+		}
+	});
 });
 
 module.exports = router;
