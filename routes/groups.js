a9b477690740130a0ab881585ca14724a9084acf
Ludovic <ludovictichit3@gmail.com>
2016-11-16 18:54:09 +0100

    [LTI]Adding dashboard to groups

--- routes/groups.js
+++ routes/groups.js
@@ -10,7 +10,6 @@ var users = require('../config').users;
 router.get('/groups/:id', function(req, res) {
     groups.get(req.params.id, function(group, err) {
         if(err) {
-            console.log(err.cause);
             res.status(err.status).send(err.cause);
         } else {
             res.status(200).json(group);
@@ -21,7 +20,6 @@ router.get('/groups/:id', function(req, res) {
 router.get('/groups', function(req, res) {
     groups.getAll(function(groups, err) {
         if(err) {
-            console.log(err.cause);
             res.status(err.status).send(err.cause);
         } else {
             res.status(200).json(groups);
@@ -32,7 +30,6 @@ router.get('/groups', function(req, res) {
 router.delete('/groups/:id', function(req, res) {
 	groups.remove(req.params.id, function(err) {
 		if(err) {
-			console.log(err.cause);
 			res.status(err.status).send(err.cause);
 		} else {
 			res.status(200).send('Group removed');
@@ -46,8 +43,7 @@ router.post('/groups/create', function(req, res) {
 	} else {
         users.exists(req.body.idAdmin, function(exists, err) {
             if(err) {
-                    console.log(err.cause);
-                    res.status(err.status).send(err.cause);
+                res.status(err.status).send(err.cause);
             } else {
                 if(exists) {
                     groups.create(req.body.name, req.body.description, req.body.idAdmin, function(group, err){
@@ -68,7 +64,6 @@ router.post('/groups/create', function(req, res) {
 router.put('/groups/join/:groupId', function(req, res) {
     users.exists(req.body.userId, function(exists, err1) {
         if(err1) {
-            console.log(err1.cause);
             res.status(err1.status).send(err1.cause);
         } else {
             if(exists) {
@@ -89,7 +84,6 @@ router.put('/groups/join/:groupId', function(req, res) {
 router.get('/groups/:id/admin', function(req, res) {
     groups.getAdmin(req.params.id, function(admin, err) {
         if(err) {
-            console.log(err.cause);
             res.status(err.status).send(err.cause);
         } else {
             res.status(200).send(admin);
@@ -134,6 +128,51 @@ router.get('/groups/members/:userId', function(req, res) {
             } else {
                 res.status(404).send('User not found');
             }
+        }
+    });
+});
+
+router.post('/groups/comments/create', function(req, res) {
+    if(!req.body.groupId || !req.body.message || !req.body.authorId) {
+		res.status(401).send('Required group/message/author');
+	} else {
+        users.exists(req.body.authorId, function(exists, err) {
+            if(err) {
+                    console.log(err.cause);
+                    res.status(err.status).send(err.cause);
+            } else {
+                if(exists) {
+                    groups.addComment(req.body.groupId, req.body.message, req.body.authorId, function(group, err){
+                        if(err) {
+                            res.status(err.status).send(err.cause);
+                        } else {
+                            res.status(200).send('Comment created');
+                        }
+                    });
+                } else {
+                    res.status(404).send('User not found');
+                }
+            }
+        });
+	}
+});
+
+router.get('/groups/:groupId/comments', function(req, res) {
+    groups.getComments(req.params.groupId, function(comments, err) {
+        if(err) {
+            res.status(err.status).send(err.cause);
+        } else {
+            res.status(200).json(comments);
+        }
+    });
+});
+
+router.get('/groups/:groupId/comments/:commentId', function(req, res) {
+    groups.getComment(req.params.groupId, req.params.commentId, function(comment, err) {
+        if(err) {
+            res.status(err.status).send(err.cause);
+        } else {
+            res.status(200).json(comment);
         }
     });
 });
